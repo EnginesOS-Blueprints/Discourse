0ce2f2ec48c84fc201c4d5911d90826a1eeecd12
James Vodanovich <james@jvodan.com>
2015-01-03 19:58:24 +1100

    ini

--- engines/scripts/install.sh
+++ engines/scripts/install.sh
@@ -3,4 +3,4 @@ wget http://download.redis.io/releases/redis-2.8.9.tar.gz;
 tar xzf redis-2.8.9.tar.gz;
 cd redis-2.8.9;
 make;
-make  PREFIX=/usr/local/redis/ install;
+make  PREFIX=/home/fs/local/redis/ install;
--- engines/scripts/start.sh
+++ engines/scripts/start.sh
@@ -1,4 +1,4 @@
-/usr/local/redis/bin/redis-server  --loglevel debug;
+/home/fs/local/redis/bin/redis-server  --loglevel debug;
 cd /home/app;
 /usr/local/rbenv/shims/bundle exec rake db:migrate;
 /usr/local/rbenv/shims/bundle exec rake db:populate;
